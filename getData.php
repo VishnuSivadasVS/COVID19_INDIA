ebf1bdce7b7c3f5ef30571e08657757101ae17a9
Vishnu Sivadas <mail@vishnusivadas.com>
2024-05-09 11:13:53 +0530

    Depreciated

--- getData.php
+++ getData.php
@@ -1,9 +1,6 @@
 <?php
 /*
  * Developer: VishnuSivadasVS
- * Website: https://www.vishnusivadas.com
- * Github: https://github.com/VishnuSivadasVS
- * API: https://staysafe.vishnusivadas.com/apis/covid19_india.php
  */
 require "simple_html_dom.php";
 header('Content-Type: application/json');
